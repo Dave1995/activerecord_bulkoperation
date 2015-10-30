c49bd923ab0b4d76197711733fc97621fd6b7149
Rico Pahlisch <rico.pahlisch@ottogroup.com>
2015-10-30 13:31:33 +0100

    RP fix endless loop

--- lib/activerecord_bulkoperation/adapters/abstract_adapter.rb
+++ lib/activerecord_bulkoperation/adapters/abstract_adapter.rb
@@ -9,6 +9,27 @@ module ActiveRecord
             alias_method :rollback_db_transaction_without_callback, :rollback_db_transaction
             alias_method :rollback_to_savepoint_without_callback, :rollback_to_savepoint
             alias_method :create_savepoint_without_callback, :create_savepoint
+
+            def commit_db_transaction
+              connection_listeners.each { |l| l.before_commit if l.respond_to?('before_commit') }          
+              commit_db_transaction_without_callback
+              connection_listeners.each { |l| l.after_commit if l.respond_to?('after_commit') }
+            end
+            
+            def rollback_db_transaction
+              rollback_db_transaction_without_callback
+              connection_listeners.each { |l| l.after_rollback if l.respond_to?('after_rollback') }
+            end
+                    
+            def rollback_to_savepoint(name = current_savepoint_name)
+              rollback_to_savepoint_without_callback
+              connection_listeners.each { |l| l.after_rollback_to_savepoint if l.respond_to?('after_rollback_to_savepoint') }
+            end
+                    
+            def create_savepoint
+              connection_listeners.each { |l| l.before_create_savepoint if l.respond_to?('before_create_savepoint') }
+              create_savepoint_without_callback
+            end
           end
         end
 
@@ -16,26 +37,6 @@ module ActiveRecord
           @connection_listeners ||= []
         end
          
-        def commit_db_transaction
-          connection_listeners.each { |l| l.before_commit if l.respond_to?('before_commit') }          
-          commit_db_transaction_without_callback
-          connection_listeners.each { |l| l.after_commit if l.respond_to?('after_commit') }
-        end
-        
-        def rollback_db_transaction
-          rollback_db_transaction_without_callback
-          connection_listeners.each { |l| l.after_rollback if l.respond_to?('after_rollback') }
-        end
-                
-        def rollback_to_savepoint(name = current_savepoint_name)
-          rollback_to_savepoint_without_callback
-          connection_listeners.each { |l| l.after_rollback_to_savepoint if l.respond_to?('after_rollback_to_savepoint') }
-        end
-                
-        def create_savepoint
-          connection_listeners.each { |l| l.before_create_savepoint if l.respond_to?('before_create_savepoint') }
-          create_savepoint_without_callback
-        end
       end
     end
   end  
--- test/active_record_connection_test.rb
+++ test/active_record_connection_test.rb
@@ -13,4 +13,8 @@ class ActiveRecordConnectionTest < ActiveSupport::TestCase
     assert ActiveRecord::Base.connection.respond_to? :rollback_to_savepoint_without_callback
     assert ActiveRecord::Base.connection.respond_to? :create_savepoint_without_callback
   end
+
+  def test_method_definition
+    ActiveRecord::Base.connection.commit_db_transaction_without_callback
+  end
 end
