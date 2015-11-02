2659b9ee2ba617c16fc301a4ada5f1335659c3fd
André Kullmann <Andre.Kullmann@ottogroup.com>
2015-11-02 17:03:29 +0100

    AK remove puts

--- lib/activerecord_bulkoperation/active_record/adapters/abstract_adapter.rb
+++ lib/activerecord_bulkoperation/active_record/adapters/abstract_adapter.rb
@@ -16,7 +16,6 @@ module ActiveRecord # :nodoc:
 
       def log
         ActiveRecord::Base.logger
-        #@log ||= AmosBase::Util::Logging::Log.get('sql')
       end
 
       def set_connection(connection)
@@ -37,7 +36,6 @@ module ActiveRecord # :nodoc:
         @timeout = 5
         connection = super_checkout
         connection.clear_scheduled_operations if connection.respond_to?('clear_scheduled_operations')
-        #logentry_login(connection)
         start_plsql_profiling(connection)
         connection
       end
@@ -45,61 +43,13 @@ module ActiveRecord # :nodoc:
       # before checkout clear the scheduled operation if necessary
       alias_method :super_checkin, :checkin
       def checkin(connection)
-        puts "checkin"
-        return unless connection
         connection.clear_scheduled_operations if connection.respond_to?('clear_scheduled_operations')
         connection.connection_listeners.each { |l| l.before_close if l.respond_to?('before_close') } if connection.respond_to?('connection_listeners')
-        logentry_logout(connection)
         stop_plsql_profiling(connection)
         super_checkin(connection)
       end
 
       private
-
-      def logentry_login(connection)
-        if connection.class.name.index('Oracle')
-          execution_id = AmosBase::Util::Context.get.execution_id || ENV['EXECUTION_ID']
-          batch_name   = AmosBase::Util::Context.get.batch_name || ENV['EXECUTION_SCRIPT']
-          opcode       = ENV['OPCODE']
-          if execution_id.present? or batch_name.present? or opcode.present?
-            if RuntimeEnv.smile?
-              execution_part = execution_id.present? ? "/*:i #{execution_id}*/ #{execution_id} /*:e*/" : '/*:i */ null /*:e*/'
-              batch_part     = batch_name.present? ? "/*:s '#{batch_name}'*/ '#{batch_name}' /*:e*/" : '/*:s */ null /*:e*/'
-              opcode_part    = opcode.present? ? "/*:s '#{opcode}'*/ '#{opcode}' /*:e*/" : '/*:s */ null /*:e*/'
-            else
-              execution_part = execution_id.present? ? "#{execution_id}" : 'null'
-              batch_part     = batch_name.present? ? "'#{batch_name}'" : 'null'
-              opcode_part    = opcode.present? ? "'#{opcode}'" : 'null'
-            end
-            sql = "BEGIN LOGENTRY.LOGIN( #{batch_part}, #{execution_part}, #{opcode_part} ); END;"
-            connection.execute(sql)
-          end
-        end
-      rescue ActiveRecord::StatementInvalid => e
-        log.debug(e.message)
-        # do nothing maybe package doesn't exists ?
-      rescue ArgumentError => e
-        log.debug(e.message)
-        # do nothing maybe package doesn't exists ?
-      rescue Exception => e
-        log.error(e)
-      end
-
-      def logentry_logout(connection)
-        if connection.class.name.index('Oracle')
-          sql = 'BEGIN LOGENTRY.LOGOUT(); END;'
-          connection.execute(sql)
-        end
-
-      rescue ActiveRecord::StatementInvalid => e
-        log.debug(e.message)
-        # do nothing maybe package doesn't exists ?
-      rescue ArgumentError => e
-        log.debug(e.message)
-        # do nothing maybe package doesn't exists ?
-      rescue Exception => e
-        log.error(e)
-      end
 
       # if we are running for pl/sql coverage
       # we need to start the profiler at checkout
