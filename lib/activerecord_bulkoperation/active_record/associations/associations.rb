0c48400caeb4eae2be65bc65654110934d31cfb2
André Kullmann <Andre.Kullmann@ottogroup.com>
2016-01-27 13:22:58 +0100

    AK

--- lib/activerecord_bulkoperation/active_record/associations/associations.rb
+++ lib/activerecord_bulkoperation/active_record/associations/associations.rb
@@ -7,7 +7,7 @@ module ActiveRecord
 
         alias_method :instantiate_without_save_original, :instantiate
 
-        def instantiate(attributes, column_types)
+        def instantiate(attributes, column_types = {})
           record = instantiate_without_save_original(attributes, column_types)
           record.save_original
           record
